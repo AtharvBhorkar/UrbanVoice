09e3bcf4d6e1855142af7a34ce3bac8d6d50c090
Atharv Bhorkar <atharvbhorkar19@gmail.com>
2026-09-06 12:22:40 +0530

    done

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,9 +12,20 @@ const categoryRoutes = require('./routes/categoryRoutes');
 
 const app = express();
 
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  'http://localhost:5173',
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   })
 );
